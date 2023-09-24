1834b16076cc6d0d47d3b2ef1f98ff601f3adaf1
texteditor <wh932S@login.missouristate.edu>
2023-09-24 15:19:02 -0500

    Homework 3 Exercise 1 (9/24)

--- app/src/main/java/com/example/hw3exercise1/MainActivity.kt
+++ app/src/main/java/com/example/hw3exercise1/MainActivity.kt
@@ -12,6 +12,9 @@ import com.example.hw3exercise1.databinding.ActivityMainBinding
 import com.google.android.material.snackbar.Snackbar
 
 
+
+
+
 //Property that the log function takes, called a TAG
 //filters down verbose logging
 //Keeps function call cleaner
@@ -42,7 +45,10 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        //First Log for on create action
+
+
+//First Log for onCreate action
+//Great way to track whats going on and finding specific values in a lot of logging
         Log.d(TAG, "onCreate (Bundle?) called")
 
         binding = ActivityMainBinding.inflate(layoutInflater)
@@ -51,8 +57,7 @@ class MainActivity : AppCompatActivity() {
 
 
 
-        //My on onclick listener for the true button
-        // and coding it with 'Correct!'
+//My on onclick listener for the true button and coding it with 'Correct!'
         binding.trueButton.setOnClickListener {
 
             checkAnswer(true)
@@ -61,8 +66,7 @@ class MainActivity : AppCompatActivity() {
 
 
 
-        //My on onclick listener for the false button
-        // and coding it with 'False!'
+//My on onclick listener for the false button and coding it with 'False!'
         binding.falseButton.setOnClickListener {
 
             checkAnswer(false)
@@ -71,14 +75,17 @@ class MainActivity : AppCompatActivity() {
 
 
 
-        //Finding questionTextView through binding
-        //Moving to next and previous questions on click
+//Finding questionTextView through binding
+//Moving to next and previous questions on click
         binding.questionTextView.setOnClickListener {
+
             currentIndex = (currentIndex + 1) % questionBank.size
+
             updateQuestion()
         }
 
         binding.previousButton.setOnClickListener {
+
             currentIndex = (currentIndex - 1) % questionBank.size
 
             updateQuestion()
@@ -91,10 +98,7 @@ class MainActivity : AppCompatActivity() {
 
             updateQuestion()
 
-            }
-
-
-
+        }
 
         updateQuestion()
     }
@@ -104,9 +108,13 @@ class MainActivity : AppCompatActivity() {
 
 
     private fun updateQuestion() {
+
         val questionTextResId = questionBank[currentIndex].textResId
+
         binding.questionTextView.setText(questionTextResId)
+
     }
+
 
 
     private fun checkAnswer(userAnswer: Boolean) {
@@ -124,15 +132,16 @@ class MainActivity : AppCompatActivity() {
         }
 
 
-
         Toast.makeText(this, messageResId, Toast.LENGTH_LONG)
             .show()
-
 
     }
 
 
+
+
 //Logs to see in console where application is at during processing
+//Overriding but leaving all pre existing functionality of onStart function
     override fun onStart() {
         super.onStart()
         Log.d(TAG, "onStart() called")
@@ -145,19 +154,16 @@ class MainActivity : AppCompatActivity() {
     }
 
 
-
     override fun onPause() {
         super.onPause()
         Log.d(TAG, "onPause() called")
     }
 
 
-
     override fun onStop() {
         super.onStop()
         Log.d(TAG, "onStop() called")
     }
-
 
 
     override fun onDestroy() {
